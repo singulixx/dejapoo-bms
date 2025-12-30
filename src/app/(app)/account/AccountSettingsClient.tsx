907f74297d4c978f635ecca205c1b21cd8430804
reyprovidence <reydityads.providence@gmail.com>
2025-12-30 11:17:32 +0700

    vvv

--- src/app/(app)/account/AccountSettingsClient.tsx
+++ src/app/(app)/account/AccountSettingsClient.tsx
@@ -136,7 +136,7 @@ export default function AccountSettingsClient() {
               type="password"
               value={currentPassword}
               onChange={(e) => setCurrentPassword(e.target.value)}
-              className="w-full rounded-lg border border-stroke bg-transparent px-4 py-2 text-dark outline-none focus:border-primary dark:border-dark-3 dark:text-white"
+              className="w-full rounded-lg border border-stroke bg-gray-2 px-4 py-2 text-dark outline-none focus:border-primary dark:border-dark-3 dark:bg-dark-2 dark:text-white"
             />
           </div>
 
@@ -149,7 +149,7 @@ export default function AccountSettingsClient() {
                 type="password"
                 value={newPassword}
                 onChange={(e) => setNewPassword(e.target.value)}
-                className="w-full rounded-lg border border-stroke bg-transparent px-4 py-2 text-dark outline-none focus:border-primary dark:border-dark-3 dark:text-white"
+                className="w-full rounded-lg border border-stroke bg-gray-2 px-4 py-2 text-dark outline-none focus:border-primary dark:border-dark-3 dark:bg-dark-2 dark:text-white"
               />
             </div>
             <div>
@@ -160,7 +160,7 @@ export default function AccountSettingsClient() {
                 type="password"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
-                className="w-full rounded-lg border border-stroke bg-transparent px-4 py-2 text-dark outline-none focus:border-primary dark:border-dark-3 dark:text-white"
+                className="w-full rounded-lg border border-stroke bg-gray-2 px-4 py-2 text-dark outline-none focus:border-primary dark:border-dark-3 dark:bg-dark-2 dark:text-white"
               />
             </div>
           </div>
